5ad80c84af3ff3467d104a1a6f7ec7e19345a2b3
lizhuo <superking_0104@163.com>
2019-03-25 16:20:55 +0800

    update: 增加默认背景色种类

--- src/components/Theme/themes/default.tsx
+++ src/components/Theme/themes/default.tsx
@@ -6,12 +6,15 @@ export default {
   text_color_2: '#999',
 
   // 背景
-  bg_color_1: '#1b1f2314',
+  bg_color_1: '#1b1f23',
+  bg_color_2: '#ccc',
 
   // 边框
   border_color_1: '#eaeaea',
   border_color_2: '#e5e5e5',
   //分割线
   //板块背景分割线
-  color_neutrals_divider1: '#eeeeee'
+  color_neutrals_divider1: '#eeeeee',
+  // 基础行高
+  list_item_height: 25
 };
